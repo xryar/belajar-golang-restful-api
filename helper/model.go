a785fb797a4011e94e5bc2be02d9d2660ca661ab
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-04-28 13:40:11 +0700

    service implementation

---
+++ helper/model.go
@@ -0,0 +1,22 @@
+package helper
+
+import (
+	"belajar-golang-retsful-api/model/domain"
+	"belajar-golang-retsful-api/model/web"
+)
+
+func ToCategoryResponse(category domain.Category) web.CategoryResponse {
+	return web.CategoryResponse{
+		Id:   category.Id,
+		Name: category.Name,
+	}
+}
+
+func ToCategoryResponses(categories []domain.Category) []web.CategoryResponse {
+	var categoryResponses []web.CategoryResponse
+	for _, category := range categories {
+		categoryResponses = append(categoryResponses, ToCategoryResponse(category))
+	}
+
+	return categoryResponses
+}
