9ce41ca326767a3dfe18f682acebc4030a735648
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-04-30 17:42:45 +0700

    add server

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"belajar-golang-retsful-api/app"
 	"belajar-golang-retsful-api/controller"
+	"belajar-golang-retsful-api/helper"
 	"belajar-golang-retsful-api/repository"
 	"belajar-golang-retsful-api/service"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/julienschmidt/httprouter"
@@ -24,4 +28,12 @@ func main() {
 	router.POST("/api/categories", categoryController.Create)
 	router.PUT("/api/categories/:categoryId", categoryController.Update)
 	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
+
+	server := http.Server{
+		Addr:    "localhost:3000",
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+	helper.PanicIfError(err)
 }
